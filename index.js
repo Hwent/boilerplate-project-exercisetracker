380bc487cdbde99b0029a559ea0172dfa6957d89
Wentao <wentaowilliam@outlook.com>
2024-04-08 12:05:56 -0400

    finish practice

--- index.js
+++ index.js
@@ -27,30 +27,51 @@ app.post("/api/users", (req, res) => {
   res.json(newUser);
 });
 
-app.get("/api/users/:_id/log", (req, res) => {
+app.get("/api/users/:_id/logs", (req, res) => {
   let id = req.params._id;
   let user = users.find((user) => user._id === id);
   let record = exercises.filter((exercise) => exercise._id === id);
+
+  // Filter based on 'from' and 'to' dates if they are provided
+  if (req.query.from || req.query.to) {
+    let fromDate = req.query.from ? new Date(req.query.from) : new Date(0); // default to 1970-01-01 if no 'from' date is provided
+    let toDate = req.query.to ? new Date(req.query.to) : new Date(); // default to current date if no 'to' date is provided
+    record = record.filter((exercise) => {
+      let exerciseDate = new Date(exercise.date);
+      return exerciseDate >= fromDate && exerciseDate <= toDate;
+    });
+  }
+
+  // Limit the number of logs if 'limit' is provided
+  if (req.query.limit) {
+    record = record.slice(0, req.query.limit);
+  }
+
   let log = {
-    username: user.username,
     _id: id,
+    username: user.username,
     count: record.length,
-    log: record,
+    log: record.map((exercise) => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date,
+    })),
   };
   res.json(log);
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   let id = req.params._id;
-  let date = new Date(req.body.date);
+  let dateInput = req.body.date;
+  let date = dateInput ? new Date(dateInput + "T00:00:00") : new Date();
   let duration = req.body.duration;
   let description = req.body.description;
   let user = users.find((user) => user._id === id);
   let exercise = {
-    username: user.username,
     _id: id,
+    username: user.username,
     date: date.toDateString(),
-    duration: duration,
+    duration: parseInt(duration),
     description: description,
   };
   exercises.push(exercise);
